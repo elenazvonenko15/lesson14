0ff07435de5836cce209970ba1c40099ba84a2f4
Elena Zvonenko <45172864+elenazvonenko15@users.noreply.github.com>
2021-06-25 20:18:06 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
+const reverseNumber = num => {
+    const numStr = String(num);
+    const firstCharIndex = 0;
+    const startIndex = 1;
+    if (!numStr) {
+        return '';
+    }
+    return Number(reverseNumber(numStr.substring(startIndex)) + numStr[firstCharIndex]);
+};
+
 function getPolindrom(originalNum) {
     let steps = 0;
-
-    const reverseNumber = num => {
-        const numStr = String(num);
-        const firstCharIndex = 0;
-        const startIndex = 1;
-        if (!numStr) {
-            return '';
-        }
-        return Number(reverseNumber(numStr.substring(startIndex)) + numStr[firstCharIndex]);
-    };
 
     function calcPolindrom(num = originalNum) {
         try {
